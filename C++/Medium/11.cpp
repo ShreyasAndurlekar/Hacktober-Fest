83476f2cf91d2426a8407ed41564942807ead2c5
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 12:59:21 +0530

    11solved

--- C++/Medium/11.cpp
+++ C++/Medium/11.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int a = 5, b = 10;
-    if (a > 0 || b < 5 && a < 10)
+    if ((a > 0 || b < 5) && a < 10) // brackets should be properly structured
     {
         cout << "Condition met" << endl;
     }
