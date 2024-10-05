911ff6653b4087e836ab95fd0dbd9c8023a6d6e8
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 12:47:24 +0530

    22.py

--- Python/Medium/22.py
+++ Python/Medium/22.py
@@ -1,8 +1,11 @@
-a=3
-b=4
-print(a+b+c)
+a = 3
+b = 4
+c = 5  # Define c
+print(a + b + c)  # This will now correctly output 12
 
 x = 5
-y = z  
+y = 10  # Define z before using it
+z = 15  # Define z
+y = z  # Now y is assigned the value of z
 
-print(x + y)
+print(x + y)  # This will print output 20 (5 + 15)
