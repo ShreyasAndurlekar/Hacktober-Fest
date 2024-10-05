ad8e550f111251170c4d94f2ae39544d9a22fc1a
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 12:37:47 +0530

    12.py

--- Python/Medium/12.py
+++ Python/Medium/12.py
@@ -1,3 +1,8 @@
 def greet(*args, **kwargs):
-    print("Hello, " + args + " " + kwargs)
+    # Join args into a single string
+    full_name = " ".join(args)
+    # If there are keyword arguments, format them
+    last_name = kwargs.get('last_name', '')
+    print(f"Hello, {full_name} {last_name}")
+
 greet("John", last_name="Doe")
