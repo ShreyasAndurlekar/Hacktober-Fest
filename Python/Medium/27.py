bb115bec9eba079eee34786bd5e6b4afc317b4b9
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 13:01:46 +0530

    27.py

--- Python/Medium/27.py
+++ Python/Medium/27.py
@@ -1,9 +1,9 @@
-age = 25 
-message = "I am " + age + " years old." 
+# First example with integer age
+age = 25
+message = "I am " + str(age) + " years old."  # Convert age to string
 print(message)
 
+# Second example with string age
 age = "25"
-
-message = "I am " + age + " years old."  
-
+message = "I am " + age + " years old."
 print(message)
