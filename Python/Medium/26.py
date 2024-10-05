33cc5ca44ad9b0420315c712249cb2665516f8ad
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 12:57:33 +0530

    26.py

--- Python/Medium/26.py
+++ Python/Medium/26.py
@@ -1,10 +1,19 @@
-def multiply:
-    Return a*b
-a=int(input(‘enter a num’))
-b=int(input(‘enter another number’))
+# Function to multiply two numbers
+def multiply(a, b):
+    return a * b
 
+# Function to square a number
 def square(num):
     return num * num
 
+# Input from the user 
+a = int(input('Enter a number: '))
+b = int(input('Enter another number: '))
 
-square  
+# Calculating the product and square
+result_multiply = multiply(a, b)
+result_square = square(a)
+
+print(f"The product of {a} and {b} is {result_multiply}")
+print(f"The square of {a} is {result_square}")
+ 
