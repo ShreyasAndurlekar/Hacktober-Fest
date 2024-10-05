cc6c8cf1fe284146a66af557139b6398403f1231
shreyas <85345896+ShreyasAndurlekar@users.noreply.github.com>
2024-10-05 13:50:39 +0530

    41.py

--- Python/Medium/41.py
+++ Python/Medium/41.py
@@ -1,4 +1,12 @@
 s = "hello world"
-result = s.split(" ")
-Result = s.split("")  
+
+# Split the string by space
+result = s.split(" ")  # This will give you a list: ['hello', 'world']
+
+# If you want to split into individual characters, use list() or split without any arguments
+result_characters = list(s)  # This will give you a list of characters: ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+
+print(result)  # Output: ['hello', 'world']
+print(result_characters)  # Output: ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+  
 
