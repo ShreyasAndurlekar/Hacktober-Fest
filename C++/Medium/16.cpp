e830b6b7822b6d7516987880fc232650fc3ed265
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 13:48:37 +0530

    16 medium C++

--- C++/Medium/16.cpp
+++ C++/Medium/16.cpp
@@ -10,6 +10,6 @@ void print(float a)
 }
 int main()
 {
-    print(5.0);
+    print(static_cast<float>(5.0)); // any decimal numbers are regarded as doubles in C++ not float so we need to epllicity state it's a float so it can find proper function
     return 0;
 }
